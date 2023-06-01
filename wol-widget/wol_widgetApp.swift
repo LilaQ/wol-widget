db2b862c5d366573853d017f1c884641ab4bd025
LilaQ <teh.lilaq@gmail.com>
2023-06-01 13:39:44 +0200

    Initial Commit

---
+++ wol-widget/wol_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  wol_widgetApp.swift
+//  wol-widget
+//
+//  Created by Jan Sallads on 01.06.23.
+//
+
+import SwiftUI
+
+@main
+struct wol_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
